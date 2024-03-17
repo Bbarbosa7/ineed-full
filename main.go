0ee6308e930e5bce16cbcf857d16aef0a103eaa7
Bbarbosa7 <brayann.wheberth@gmail.com>
2024-03-17 00:46:56 -0300

    FEAT: Inclue air, the hot reload for Go apps.

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/Bbarbosa7/ineed-full/app/service-search/handlers"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
@@ -11,13 +11,14 @@ func main() {
 
 	// API routes
 	r.GET("/service-types", handlers.GetServiceTypesHandler)
-	r.Run(":1234")
 
 	// Server the frontend for the Application
 	r.Static("/static", "./client/assets")
 	r.LoadHTMLGlob("client/*.html")
 	r.GET("/", func(c *gin.Context) {
-		// Código para renderizar a página inicial (index.html)
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
+
+	// Run the server
+	r.Run(":1234")
 }
